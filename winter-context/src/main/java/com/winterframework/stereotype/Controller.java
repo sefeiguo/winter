bbac4a494e90c4b94296643017eceaaced6d9a87
黄文华 <hwh>
2021-07-05 15:04:46 +0800

    RF# 增加AliasFor注解

--- winter-context/src/main/java/com/winterframework/stereotype/Controller.java
+++ winter-context/src/main/java/com/winterframework/stereotype/Controller.java
@@ -3,12 +3,27 @@
  */
 package com.winterframework.stereotype;
 
+import com.winterframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author huangwh@paraview.cn
  * @since 2021/07/05
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
 public @interface Controller {
 
-    //@AliasFor(annotation = Component.class)
+    /**
+     * @return
+     */
+    @AliasFor(annotation = Component.class)
     String value() default "";
 }
